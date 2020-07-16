97adf8b4565b9ab064212fcdd48febd62ba86c0b
nitin katiyar <nkatiyar11@gmail.com>
2020-07-16 23:15:01 +0530

    Used method for computing EmployeeWage

---
+++ Day21EmpWage.java
@@ -0,0 +1,53 @@
+class Day21EmpWage 
+   {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    
+
+
+   public static void empComputation() 
+ 
+{ 
+
+ System.out.println("Welcome to Employee Wage Computation program");
+    
+    int numOfWorkingDays = 20;
+    int maxHrsInMonth = 100;    
+    int empHrs = 0;
+    int empWage = 0;
+    int totalEmpHrs=0;
+    int totalWorkingDays=0;
+    
+while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays <= numOfWorkingDays)
+  {
+     totalWorkingDays++;
+     int  empCheck  = (int) Math.floor(Math.random() * 10) % 2;
+     switch(empCheck)
+        {
+             case IS_PART_TIME:
+               empHrs = 4 ;
+                 break;
+             case IS_FULL_TIME:
+               empHrs = 8;
+                 break;
+    default:
+         empHrs = 0;
+      } 
+     totalEmpHrs+=empHrs;
+
+}
+     empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+
+    System.out.println ("Employee Wage: "+ empWage);
+
+}
+
+   public static void main(String[] args) 
+  {
+        empComputation();
+   
+   }
+}   
+
+
