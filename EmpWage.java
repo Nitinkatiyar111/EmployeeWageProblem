1f4a6de9442328b7fcc45b5f96f16cb885957e4e
nitin katiyar <nkatiyar11@gmail.com>
2020-07-16 22:25:59 +0530

    Added condition for day Hours

--- EmpWage.java
+++ EmpWage.java
@@ -1,19 +1,26 @@
-class EmpWage {
-        public static void main(String[] args) 
-  {
-     
-System.out.println("Welcome to Employee Wage Computation program");
-
+class EmpWage 
+   {
+    public static void main(String[] args) 
+  
+   {
+    System.out.println("Welcome to Employee Wage Computation program");
+    
     int IS_FULL_TIME = 1;
     int IS_PART_TIME = 2;
     int EMP_RATE_PER_HOUR = 20;
-    int totalWorkingDay = 20;
-
-     int empHrs = 0;
-     int empWage = 0;
-  
+    int numOfWorkingDays = 20;
+    int maxHrsInMonth = 100;
+    int empHrs = 0;
+    int empWage = 0;
+    int totalEmpHrs=0;
+    int totalWorkingDays=0;
+    
+while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays <= numOfWorkingDays)
+  {
+     totalWOrkingDays++;
      int  empCheck  = (int) Math.floor(Math.random() * 10) % 2;
      switch(empCheck)
+        {
              case IS_PART_TIME:
                empHrs = 4 * totalWorkingDay ;
                  break;
@@ -23,7 +30,13 @@ System.out.println("Welcome to Employee Wage Computation program");
     default:
          empHrs = 0;
       } 
-     empWage = empHrs * EMP_RATE_PER_HOUR;
+     totalEmpHrs+=empHrs;
+
+}
+     empWage = totalEMpHrs * EMP_RATE_PER_HOUR;
+
     System.out.prinltn ("Employee Wage: "+ empWage);
    }
 }   
+
+
