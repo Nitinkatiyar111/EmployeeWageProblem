789335349748d435edc0d533bb7958aa6a9b303a
nitin katiyar <nkatiyar11@gmail.com>
2020-07-16 22:00:07 +0530

    Refactor code using switch case

--- EmpWage.java
+++ EmpWage.java
@@ -12,13 +12,16 @@ System.out.println("Welcome to Employee Wage Computation program");
      int empWage = 0;
   
      int  empCheck  = (int) Math.floor(Math.random() * 10) % 2;
-     if (empCheck == IS_FULL_TIME)
-            empHrs = 8;
-     else if (empCheck == IS_PART_TIME)
-           empHrs = 4;
-
-      else
-           empHrs = 0;
+     switch(empCheck)
+             case IS_PART_TIME:
+               empHrs = 4 ;
+                 break;
+             case IS_FULL_TIME:
+               empHrs = 8;
+                 break;
+    default:
+         empHrs = 0;
+      } 
      empWage = empHrs * EMP_RATE_PER_HOUR;
     System.out.prinltn ("Employee Wage: "+ empWage);
    }
