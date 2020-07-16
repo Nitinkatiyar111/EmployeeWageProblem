676e739395b41c88c451a6afe596f5ccaea4de74
nitin katiyar <nkatiyar11@gmail.com>
2020-07-16 21:51:30 +0530

    added part time functionality

--- EmpWage.java
+++ EmpWage.java
@@ -5,6 +5,7 @@ class EmpWage {
 System.out.println("Welcome to Employee Wage Computation program");
 
     int IS_FULL_TIME = 1;
+    int IS_PART_TIME = 2;
     int EMP_RATE_PER_HOUR = 20;
 
      int empHrs = 0;
@@ -13,9 +14,11 @@ System.out.println("Welcome to Employee Wage Computation program");
      int  empCheck  = (int) Math.floor(Math.random() * 10) % 2;
      if (empCheck == IS_FULL_TIME)
             empHrs = 8;
+     else if (empCheck == IS_PART_TIME)
+           empHrs = 4;
 
       else
-            empHrs = 4;
+           empHrs = 0;
      empWage = empHrs * EMP_RATE_PER_HOUR;
     System.out.prinltn ("Employee Wage: "+ empWage);
    }
