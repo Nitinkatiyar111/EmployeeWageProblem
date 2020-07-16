ea27a90849d4fca9e883f601f2e7f7a4eb08dd74
nitin katiyar <nkatiyar11@gmail.com>
2020-07-16 22:08:39 +0530

    Added 20 days EmpWage

--- EmpWage.java
+++ EmpWage.java
@@ -7,6 +7,7 @@ System.out.println("Welcome to Employee Wage Computation program");
     int IS_FULL_TIME = 1;
     int IS_PART_TIME = 2;
     int EMP_RATE_PER_HOUR = 20;
+    int totalWorkingDay = 20;
 
      int empHrs = 0;
      int empWage = 0;
@@ -14,10 +15,10 @@ System.out.println("Welcome to Employee Wage Computation program");
      int  empCheck  = (int) Math.floor(Math.random() * 10) % 2;
      switch(empCheck)
              case IS_PART_TIME:
-               empHrs = 4 ;
+               empHrs = 4 * totalWorkingDay ;
                  break;
              case IS_FULL_TIME:
-               empHrs = 8;
+               empHrs = 8 * totalWorkingDay ;
                  break;
     default:
          empHrs = 0;
